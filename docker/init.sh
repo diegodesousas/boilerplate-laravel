d629a63fc9f424c5d5c7fcf083c08faa8976260b
Diego <diegodesousas@yahoo.com.br>
2017-01-02 14:13:53 -0200

    Finalizando script de atualização do container.

--- docker/init.sh
+++ docker/init.sh
@@ -3,8 +3,11 @@
 git pull origin master
 composer install
 php artisan migrate
+service php7.0-fpm start
+service nginx start
 
 # keep running
 while :; do
+  php artisan inspire
   sleep 300
 done
